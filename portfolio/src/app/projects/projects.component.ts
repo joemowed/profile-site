a3f65a08e9f44ad8dae58b2ae2c5c84f8894a986
joe <maloney.jg2@gmail.com>
2024-02-27 13:06:06 -0500

    finished applied experience

--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -13,6 +13,22 @@ export class ProjectsComponent implements OnInit {
 
   readonly projects: Project[] = [
     {
+      heading: 'Follower Counter',
+      card: [{
+        subheading: 'Device',
+        description: 'This device gets the number of followers/subscribers someone has and displays the number on a large 7-segment display.  It uses an ESP32 to control the display and communicate over the internet.',
+        imageRelUrl: '../../assets/subc-q.png',
+      }, {
+        subheading: 'System Architecture',
+        description: 'To increase the speed new features (e.g. supporting new social media platforms) can be implemented, each device has a unique ID corresponding to a database entry containing the number to display.  This number is updated by a google cloud instance communicating with social media APIs, based on a users web based device configuration.  This eliminates the need for OTA updates.',
+        imageRelUrl: '../../assets/subCDiagram-q.png',
+      }, {
+        subheading: 'What I Learned',
+        description: 'System architecture plays a large role in determining the complexity of a product.  Good system architecture can increase the speed of creating a MVP, by eliminating time spent on developing complex ways to achieve something that can be done a simpler way.',
+        imageRelUrl: '../../assets/SubCLayout-q.png',
+      }]
+    },
+    {
       heading: 'FPGA Dev Board',
       card: [{
         subheading: 'Purpose',
@@ -31,11 +47,44 @@ export class ProjectsComponent implements OnInit {
       }
       ]
     },
+    {
+      heading: 'Matrix Display Clock',
+      card: [
+        {
+          subheading: 'Design',
+          description: 'Using an CORTEX-M4 MCU, an I2C RTC, and a 64x32 RGB led matrix, I made a alarm clock.  This was done first inside the arduino framework, and then later without the arduino framework.',
+          imageRelUrl: '../../assets/clock-q.png'
+
+        }, {
+          subheading: 'Initialization',
+          description: 'When writing the linker file and reset vector functions, the bug to the left was insiduous.  GCC optimized out writing function references to the reset vector table on anything other than -O0.',
+          imageRelUrl: '../../assets/noOpt-q.png'
+
+        }, {
+          subheading: 'What I Learned',
+          description: 'Programming an MCU from the linker file upwards taught me alot about how code in a text editor becomes ones and zeros that can be written to flash storage and ran on a MCU, as well as about how memory mapped registers and I/O control all aspects of a MCU.',
+          imageRelUrl: '../../assets/240Meg-q.png'
+
+        },
+      ]
+
+    }, {
+      heading: "Misc",
+      card: [{
+        subheading: 'Tiny Thermometer',
+        description: 'Designed a tiny (3cmx4cm) ESP32 and thermocouple based thermometer.',
+        imageRelUrl: '../../assets/tinyTemp-q.png'
+      }, {
+        subheading: 'Machine Learning (in progress)',
+        description: 'I am currently working on a basic, derivative based machine learning algorithm, implemented from scratch in c++.  This will serve as a foundation to learning more complex algorithms in the future.',
+        imageRelUrl: '../../assets/mlone-q.png'
+      },]
+    }
 
 
   ]
 
-  activeProject = this.projects[0].heading
+  activeProject = this.projects[1].heading
   isMobile: boolean = false;
   menuActive: boolean = false;
   ngOnInit(): void {
