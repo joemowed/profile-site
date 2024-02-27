fae8ccf5459f59a4e1bbd0882c216230e6fbe8da
joe <maloney.jg2@gmail.com>
2024-02-26 20:28:18 -0500

    application

---
+++ portfolio/src/app/projects/project/project.component.ts
@@ -0,0 +1,54 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '../projects.component';
+import { ProgressiveImageLoaderDirective } from '../../progressive-image.directive';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project',
+  standalone: true,
+  imports: [ProgressiveImageLoaderDirective, CommonModule],
+  templateUrl: './project.component.html',
+  styleUrl: './project.component.css'
+})
+export class ProjectComponent implements OnInit {
+  @Input({ required: true }) project!: Project;
+  activeCardDesc?: string
+  constructor() {
+  }
+  ngOnInit(): void {
+
+    this.activeCardDesc = this.project.card[0].description;
+  }
+  changeViaRightArrow() {
+    const currIndex = this.getIndex(this.activeCardDesc!)
+    let changeIndex = currIndex + 1;
+    if (currIndex == this.project.card.length - 1) {
+      changeIndex = 0;
+    }
+    this.activeCardDesc = this.project.card[changeIndex].description;
+  }
+  changeViaLeftArrow() {
+    const currIndex = this.getIndex(this.activeCardDesc!)
+    let changeIndex = currIndex - 1;
+    if (changeIndex == -1) {
+      changeIndex = this.project.card.length - 1;
+    }
+
+    this.activeCardDesc = this.project.card[changeIndex].description;
+  }
+  private getIndex(desc: string): number {
+    let i = 0;
+    for (const each of this.project.card) {
+      if (each.description == desc) {
+        return i
+      }
+      i++;
+    }
+    console.log("cant find index of card desc")
+    return 0;
+  }
+  changeActiveCard(desc: string) {
+    this.activeCardDesc = desc;
+  }
+
+}
--- portfolio/src/app/projects/projects.component.ts
+++ portfolio/src/app/projects/projects.component.ts
@@ -1,12 +1,136 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
-export class ProjectsComponent {
+export class ProjectsComponent implements OnInit {
+
+  readonly projects: Project[] = [
+    {
+      heading: 'Subsciber Counter',
+      card: [{
+        subheading: 'page 1',
+        description: 'desc uno',
+        imageRelUrl: '../../assets/fpga-q.png'
+
+      }
+
+        , {
+        subheading: 'page 2',
+        description: 'desc dos',
+        imageRelUrl: '../../assets/fpga-q.png'
+
+      }
+
+
+      ]
+    }, {
+      heading: 'FPGA Dev Board',
+      card: [{
+        subheading: 'Purpose',
+        description: 'Designed a Spartan-7 based FPGA development board.  After taking a logic design class, I was curious as to how the board we were using worked, so in an effort to learn more I designed one myself.',
+        imageRelUrl: '../../assets/fpga-q.png'
+
+      }, {
+        subheading: 'Design',
+        description: 'In my design I included a boutique DC-DC converter as well as a properly isolated AC signal pathway.',
+        imageRelUrl: '../../assets/fpga2d-q.png'
+      }
+        , {
+        subheading: 'What I Learned',
+        description: 'I became much more competent at transforming information on a datasheet to traces on a PCB.',
+        imageRelUrl: '../../assets/fpgaSchematic-q.png'
+      }
+      ]
+    },
+    {
+      heading: 'Subsciber Couter',
+      card: [{
+        subheading: 'page 1',
+        description: 'desc uno',
+        imageRelUrl: '../../assets/fpga-q.png'
+
+      }]
+    }, {
+      heading: 'Suber Counter',
+      card: [{
+        subheading: 'page 1',
+        description: 'desc uno',
+        imageRelUrl: '../../assets/fpga-q.png'
+
+      }]
+    }
+
+
+
+
+  ]
+
+  activeProject = this.projects[0].heading
+  isMobile: boolean = false;
+  menuActive: boolean = false;
+  ngOnInit(): void {
+    if (window.innerWidth < 1024) {
+      this.isMobile = true
+    }
+  }
+  inHeading(heading: string) {
+    const info = this.getIndexInfo(heading);
+    if (window.innerWidth > 1024) {
+      this.projects.forEach((each) => {
+        if (each.heading != heading) {
+          //     document.getElementById(each.heading)?.classList.add('text-white/0')
+        }
+      });
+    }
+  }
+  outHeading(heading: string) {
+    this.projects.forEach((each) => {
+      //  document.getElementById(each.heading)?.classList.remove('text-white/0')
+    });
+
+  }
+  getIndexInfo(heading: string): { indexOf: number, size: number } {
+    const def = {
+      indexOf: 0,
+      size: 1
+    }
+    let i = 0;
+    def.size = this.projects.length;
+    for (; i < def.size; i++) {
+      if (this.projects[i].heading == heading) {
+        def.indexOf = i;
+        break;
+      }
+    }
+    return def
+
+  }
+
+  changeActive(heading: string) {
+    this.activeProject = heading;
+    if (this.isMobile) {
+      this.menuActive = false;
+    }
+  }
+  showMenu() {
+
+    this.menuActive = !this.menuActive;
+
+  }
 
 }
+export interface Project {
+  heading: string,
+  card: {
+    subheading: string,
+    description: string,
+    imageRelUrl: string
+  }[]
+};
