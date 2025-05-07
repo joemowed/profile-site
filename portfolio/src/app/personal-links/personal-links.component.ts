0487885b0a38a968c62cc433e083d7f7ed993c77
Joe Maloney <maloney.jg2@gmail.com>
2025-05-07 11:26:22 -0400

    added links, fixed profile image CSS

---
+++ portfolio/src/app/personal-links/personal-links.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-links',
+  imports: [],
+  templateUrl: './personal-links.component.html',
+  styleUrl: './personal-links.component.css'
+})
+export class PersonalLinksComponent {
+
+}
